2ea3e1400a4ec028ab183418522c30d637c7f997
laizhihao <tbyouth11@gmail.com>
2022-03-02 02:33:28 +0800

    feat: 三角形增加纹理demo

---
+++ Texture/shader/shader.fs
@@ -0,0 +1,11 @@
+#version 330 core
+// in vec3 ourPosition;
+in vec3 ourColor;
+in vec2 TexCoord;
+
+out vec4 color;
+uniform sampler2D ourTexCoord;
+void main()
+{
+	color = texture(ourTexCoord, TexCoord) * vec4(ourColor, 1.0f); // 纹理颜色和顶点颜色混合
+};
