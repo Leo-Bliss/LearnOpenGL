8b3b99a4938c11121af61f50b686a1e039451110
laizhihao <tbyouth11@gmail.com>
2022-03-04 00:08:05 +0800

    feat: 重新包装stb_image头文件

---
+++ Components/Common/image.h
@@ -0,0 +1,4 @@
+#pragma  once
+// popular single header image loading library 
+#define STB_IMAGE_IMPLEMENTATION // must: only contains the relevant definition source code
+#include "stb/stb_image.h"
