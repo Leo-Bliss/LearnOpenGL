d43f0ce5fc44cdf748f318e6af2d70cc3d445a00
laizhihao <tbyouth11@gmail.com>
2022-03-24 23:40:17 +0800

    test: 多光源组合
    注意定义struct对象前不要忘记了加uniform!

---
+++ LightMultiple/main.cpp
@@ -0,0 +1,286 @@
+﻿#include "window.h"
+#include "image.h"
+#include "texture.h"
+#include "shader.h"
+#include "camera.h"
+#include "vertex_array.h"
+#include "vertex_buffer.h"
+
+
+namespace Hub
+{
+	int windowWidth = 800;
+	int windowHeight = 600;
+	float deltaTime = 0.0f; // 存储上一帧渲染所用时间（当前时间戳-上一帧时间戳）
+	float lastFrame = 0.0f; // 上一帧时间戳
+
+	Camera camera;
+	glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
+
+	// 实现键盘移动摄像机
+	void processInput(GLFWwindow* window)
+	{
+		const float cameraSpeed = 5.0f * deltaTime; // 保证摄像机移动速度和硬件无关
+
+		if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+		{
+			camera.processKeyBoard(CameraMovement::FORWORD, deltaTime);
+		}
+		if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+		{
+			camera.processKeyBoard(CameraMovement::BACKWORD, deltaTime);
+		}
+		if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+		{
+			camera.processKeyBoard(CameraMovement::LEFT, deltaTime);
+		}
+		if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+		{
+			camera.processKeyBoard(CameraMovement::RIGHT, deltaTime);
+		}
+	}
+
+	// 鼠标控制摄像机
+	bool firstMouse = true;
+	glm::vec2 lastPos(windowWidth / 2.0f, windowHeight / 2.0f); // window center
+
+	void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+	{
+		if (!(glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS))
+		{
+			return;
+		}
+		glm::vec2 pos(xpos, ypos);
+		if (firstMouse)
+		{
+			lastPos = pos;
+			firstMouse = false;
+		}
+		GLfloat xoffset = pos.x - lastPos.x;
+		GLfloat yoffset = lastPos.y - pos.y; // 由于这里y坐标范围从下往上
+		lastPos = pos;
+		camera.processMouseMovement(xoffset, yoffset);
+	}
+
+	// 滚轮调整摄像机fov, 实现缩放效果
+	void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+	{
+		camera.processMouseScroll(yoffset);
+	}
+
+	void test()
+	{
+		Window hWindow(windowWidth, windowHeight);
+		auto window = hWindow.getGLWindowIns();
+
+		Shader lightShader("./shader/light.vs", "./shader/light.fs");
+		Shader lampShader("./shader/lamp.vs", "./shader/lamp.fs");
+
+		//cube
+		float vertices[] = {
+			// positions          // normals           // texture coords
+			-0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f, 0.0f,
+			 0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f, 0.0f,
+			 0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f, 1.0f,
+			 0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f, 1.0f,
+			-0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f, 1.0f,
+			-0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f, 0.0f,
+
+			-0.5f, -0.5f,  0.5f,  0.0f,  0.0f, 1.0f,   0.0f, 0.0f,
+			 0.5f, -0.5f,  0.5f,  0.0f,  0.0f, 1.0f,   1.0f, 0.0f,
+			 0.5f,  0.5f,  0.5f,  0.0f,  0.0f, 1.0f,   1.0f, 1.0f,
+			 0.5f,  0.5f,  0.5f,  0.0f,  0.0f, 1.0f,   1.0f, 1.0f,
+			-0.5f,  0.5f,  0.5f,  0.0f,  0.0f, 1.0f,   0.0f, 1.0f,
+			-0.5f, -0.5f,  0.5f,  0.0f,  0.0f, 1.0f,   0.0f, 0.0f,
+
+			-0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  1.0f, 0.0f,
+			-0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  1.0f, 1.0f,
+			-0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  0.0f, 1.0f,
+			-0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  0.0f, 1.0f,
+			-0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  0.0f, 0.0f,
+			-0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  1.0f, 0.0f,
+
+			 0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  1.0f, 0.0f,
+			 0.5f,  0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  1.0f, 1.0f,
+			 0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  0.0f, 1.0f,
+			 0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  0.0f, 1.0f,
+			 0.5f, -0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  0.0f, 0.0f,
+			 0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  1.0f, 0.0f,
+
+			-0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  0.0f, 1.0f,
+			 0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  1.0f, 1.0f,
+			 0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  1.0f, 0.0f,
+			 0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  1.0f, 0.0f,
+			-0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  0.0f, 0.0f,
+			-0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  0.0f, 1.0f,
+
+			-0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 1.0f,
+			 0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  1.0f, 1.0f,
+			 0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  1.0f, 0.0f,
+			 0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  1.0f, 0.0f,
+			-0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 0.0f,
+			-0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 1.0f
+		};
+
+		const char* filePath = "../Asset/container2.png";
+		auto image1 = Image::create(filePath);
+		auto diffuseMap = Texture::create(image1);
+		diffuseMap->setWrapping(Wrapping::axis_t::S, Wrapping::Repeat);
+		diffuseMap->setWrapping(Wrapping::axis_t::T, Wrapping::Repeat);
+		diffuseMap->setFilter(Filter::operator_t::Min, Filter::filter_t::LinearMipmapLinear);
+		diffuseMap->setFilter(Filter::operator_t::Mag, Filter::filter_t::Linear);
+
+		filePath = "../Asset/container2_specular.png";
+		auto image2 = Image::create(filePath);
+		auto specularMap = Texture::create(image2);
+		specularMap->setWrapping(Wrapping::axis_t::S, Wrapping::wrapping_t::Repeat);
+		specularMap->setWrapping(Wrapping::axis_t::T, Wrapping::wrapping_t::Repeat);
+		specularMap->setFilter(Filter::operator_t::Min, Filter::filter_t::LinearMipmapLinear);
+		specularMap->setFilter(Filter::operator_t::Mag, Filter::filter_t::Linear);
+
+		auto VBO = VertexBuffer::create(vertices, sizeof(vertices), BufferUsage::StaticDraw);
+		auto cubeVAO = VertexArray::create();
+		cubeVAO->bindAttribute(0, 3, *VBO, Type::Float, 8 * sizeof(float), 0 * sizeof(float));
+		cubeVAO->bindAttribute(1, 3, *VBO, Type::Float, 8 * sizeof(float), 3 * sizeof(float));
+		cubeVAO->bindAttribute(2, 2, *VBO, Type::Float, 8 * sizeof(float), 6 * sizeof(float));
+
+		auto lightVAO = VertexArray::create();
+		lightVAO->bindAttribute(0, 3, *VBO, Type::Float, 8 * sizeof(float), 0 * sizeof(float));
+
+		glEnable(GL_DEPTH_TEST);
+		glfwSetCursorPosCallback(window, mouse_callback);
+		glfwSetScrollCallback(window, scroll_callback);
+
+		// positions all containers
+		std::vector<glm::vec3> cubePositions = {
+			glm::vec3(0.0f,  0.0f,  0.0f),
+			glm::vec3(2.0f,  5.0f, -15.0f),
+			glm::vec3(-1.5f, -2.2f, -2.5f),
+			glm::vec3(-3.8f, -2.0f, -12.3f),
+			glm::vec3(2.4f, -0.4f, -3.5f),
+			glm::vec3(-1.7f,  3.0f, -7.5f),
+			glm::vec3(1.3f, -2.0f, -2.5f),
+			glm::vec3(1.5f,  2.0f, -2.5f),
+			glm::vec3(1.5f,  0.2f, -1.5f),
+			glm::vec3(-1.3f,  1.0f, -1.5f)
+		};
+		// positions of the point lights
+		std::vector<glm::vec3> pointLightPositions = {
+			glm::vec3(0.7f,  0.2f,  2.0f),
+			glm::vec3(2.3f, -3.3f, -4.0f),
+			glm::vec3(-4.0f,  2.0f, -12.0f),
+			glm::vec3(0.0f,  0.0f, -3.0f)
+		};
+
+		while (!hWindow.shouldClose())
+		{
+			GLfloat currentFrame = glfwGetTime();
+			deltaTime = currentFrame - lastFrame;
+			lastFrame = currentFrame;
+
+			glfwPollEvents();
+			processInput(window);
+
+			glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+			auto viewPos = camera.getPosition();
+			lightShader.setVec3("viewPos", viewPos);
+
+
+			lightShader.use();
+			glActiveTexture(GL_TEXTURE0);
+			glBindTexture(GL_TEXTURE_2D, *diffuseMap);
+			lightShader.setInt("material.diffuse", 0);
+			glActiveTexture(GL_TEXTURE1);
+			glBindTexture(GL_TEXTURE_2D, *specularMap);
+			lightShader.setInt("material.specular", 1);
+
+			lightShader.setVec3("material.specular", 0.5f, 0.5f, 0.5f);
+			lightShader.setFloat("material.shininess", 64.f);
+
+			// directional light
+			lightShader.setVec3("dirLight.direction", -0.2f, -1.0f, -0.3f);
+			lightShader.setVec3("dirLight.ambient", 0.2f, 0.2f, 0.2f);
+			lightShader.setVec3("dirLight.diffuse", 0.5f, 0.5f, 0.5f);
+			lightShader.setVec3("dirLight.specular", 1.0f, 1.0f, 1.0f);
+
+			// point lights
+			for (auto i = 0; i < 4; ++i)
+			{
+				auto id = std::to_string(i);
+				auto aim = "pointLights[" + id + "].";
+				lightShader.setVec3((aim + "position").c_str(), pointLightPositions[i]);
+
+				lightShader.setVec3((aim + "ambient").c_str(), 0.05f, 0.05f, 0.05f);
+				lightShader.setVec3((aim + "diffuse").c_str(), 0.8f, 0.8f, 0.8f);
+				lightShader.setVec3((aim + "specular").c_str(), 1.0f, 1.0f, 1.0f);
+
+				lightShader.setFloat((aim + "constant").c_str(), 1.0f);
+				lightShader.setFloat((aim + "linear").c_str(), 0.09f);
+				lightShader.setFloat((aim + "quadratic").c_str(), 0.032f);
+			}
+
+			/*lightPos.x = 2.0f * sin(glfwGetTime());
+			lightPos.y = 0.0f;
+			lightPos.z = 1.5f * cos(glfwGetTime());*/
+			// spotLight
+			lightShader.setVec3("spotLight.position", viewPos);
+			auto spotDir = camera.getFront();
+			lightShader.setVec3("spotLight.direction", spotDir);
+
+			lightShader.setVec3("spotLight.ambient", 0.05f, 0.05f, 0.05f);
+			lightShader.setVec3("spotLight.diffuse", 0.8f, 0.8f, 0.8f);
+			lightShader.setVec3("spotLight.specular", 1.0f, 1.0f, 1.0f);
+
+			lightShader.setFloat("spotLight.cutOff", glm::cos(glm::radians(12.5f)));
+			lightShader.setFloat("spotLight.outerCutOff", glm::cos(glm::radians(17.5f)));
+
+			lightShader.setFloat("spotLight.constant", 1.0f);
+			lightShader.setFloat("spotLight.linear", 0.022f);
+			lightShader.setFloat("spotLight.quadratic", 0.019f);
+			
+
+			auto model = glm::mat4(1.0f);
+			auto view = camera.getViewMatrix();
+			auto projection = camera.getProjectionMatrix(windowWidth / windowHeight * 1.0f);
+
+			lightShader.setMatirx4("view", view);
+			lightShader.setMatirx4("projection", projection);
+			glBindVertexArray(*cubeVAO);
+			for (size_t i = 0; i < cubePositions.size(); ++i)
+			{
+				glm::mat4 model(1.0f);
+				model = glm::translate(model, cubePositions[i]);
+				float angle = 20.0f * i;
+				model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+				lightShader.setMatirx4("model", model);
+				glDrawArrays(GL_TRIANGLES, 0, 36);
+			}
+			glBindVertexArray(0);
+
+			lampShader.use();
+			lampShader.setMatirx4("view", view);
+			lampShader.setMatirx4("projection", projection);
+			glBindVertexArray(*lightVAO);
+			for (size_t i = 0; i < 4; ++i)
+			{
+				model = glm::mat4(1.0f);
+				model = glm::translate(model, pointLightPositions[i]);
+				model = glm::scale(model, glm::vec3(0.2f));
+				lampShader.setMatirx4("model", model);
+				glDrawArrays(GL_TRIANGLES, 0, 36);
+			}
+			glBindVertexArray(0);
+
+			glfwSwapBuffers(window);
+		}
+		glfwTerminate();
+	}
+}
+
+int main()
+{
+	Hub::test();
+	return 0;
+}
---
+++ LightMultiple/shader/light.fs
@@ -0,0 +1,141 @@
+﻿#version 330 core
+
+out vec4 Fragcolor;
+in vec3 Normal;
+in vec3 FragPos;
+in vec2 TexCoords;
+
+uniform vec3 viewPos;
+
+struct Material
+{
+	sampler2D diffuse;
+	sampler2D specular;
+	float shininess;
+
+};
+uniform Material material;
+
+struct DirLight
+{
+	vec3 direction;
+
+	vec3 ambient;
+	vec3 diffuse;
+	vec3 specular;
+};
+uniform DirLight dirLight;
+
+vec3 CalcDirLight(DirLight light, vec3 normal, vec3 viewDir)
+{
+	vec3 lightDir = normalize(-light.direction); // 片段指向光源
+	// 漫反射强度
+	float diff = max(dot(normal, lightDir), 0.0f);
+	// 镜面反射强度
+	vec3 reflectDir = reflect(-lightDir, normal);
+	float spec = pow(max(dot(viewDir, reflectDir), 0.0f), material.shininess);
+	// 影响因素叠加
+	vec3 ambient = light.ambient * texture(material.diffuse, TexCoords).rgb;
+	vec3 diffuse = light.diffuse * diff * texture(material.diffuse, TexCoords).rgb;
+	vec3 specular = light.specular * spec * texture(material.specular, TexCoords).rgb;
+
+	return (ambient + diffuse + specular);
+}
+
+struct PointLight
+{
+	vec3 position;
+
+	float constant;
+	float linear;
+	float quadratic;
+
+	vec3 ambient;
+	vec3 diffuse;
+	vec3 specular;
+
+};
+#define NR_POINT_LIGHTS 4
+uniform PointLight pointLights[NR_POINT_LIGHTS];
+
+vec3 CalcPointLight(PointLight light, vec3 normal, vec3 fragPos, vec3 viewDir)
+{
+	vec3 lightDir = normalize(light.position - fragPos);
+	// 漫反射强度
+	float diff = max(dot(normal, lightDir), 0.0f);
+	// 镜面反射强度
+	vec3 reflectDir = reflect(-lightDir, normal);
+	float spec = pow(max(dot(viewDir, reflectDir), 0.0f), material.shininess);
+	// 按照距离衰减
+	float distance = length(light.position - FragPos);
+	float attenuation = 1.0f / (light.constant + light.linear * distance + light.quadratic * (distance * distance));
+	// 影响因素叠加
+	vec3 ambient = light.ambient * texture(material.diffuse, TexCoords).rgb;
+	vec3 diffuse = light.diffuse * diff * texture(material.diffuse, TexCoords).rgb;
+	vec3 specular = light.specular * spec * texture(material.specular, TexCoords).rgb;
+	ambient *= attenuation;
+	diffuse *= attenuation;
+	specular *= attenuation;
+	return (ambient + diffuse + specular);
+}
+
+struct SpotLight
+{
+	vec3 position;
+	vec3 direction;
+	float cutOff;
+	float outerCutOff;
+
+	float constant;
+	float linear;
+	float quadratic;
+
+	vec3 ambient;
+	vec3 diffuse;
+	vec3 specular;
+};
+uniform SpotLight spotLight;
+
+vec3 CalcSpotLight(SpotLight light, vec3 normal, vec3 fragPos, vec3 viewDir)
+{
+	vec3 lightDir = normalize(light.position - fragPos);
+	// 漫反射强度
+	float diff = max(dot(normal, lightDir), 0.0f);
+	// 镜面反射强度
+	vec3 reflectDir = reflect(-lightDir, normal);
+	float spec = pow(max(dot(viewDir, reflectDir), 0.0f), material.shininess);
+	// 按照距离衰减
+	float distance = length(light.position - FragPos);
+	float attenuation = 1.0f / (light.constant + light.linear * distance + light.quadratic * (distance * distance));
+	// 聚光平滑插值
+	float theta = dot(-lightDir, normalize(light.direction));
+	float intensity = (theta - light.outerCutOff) / (light.cutOff - light.outerCutOff);
+	intensity = clamp(intensity, 0.0f, 1.0f); 
+
+	// 影响因素叠加
+	vec3 ambient = light.ambient * texture(material.diffuse, TexCoords).rgb;
+	vec3 diffuse = light.diffuse * diff * texture(material.diffuse, TexCoords).rgb;
+	vec3 specular = light.specular * spec * texture(material.specular, TexCoords).rgb;
+	ambient *= attenuation * intensity;
+	diffuse *= attenuation * intensity;
+	specular *= attenuation * intensity;
+	return (ambient + diffuse + specular);
+}
+
+
+void main()
+{
+	vec3 norm = normalize(Normal);
+	vec3 viewDir = normalize(viewPos - FragPos);
+
+	vec3 result = CalcDirLight(dirLight, norm, viewDir);
+
+	for(int i = 0; i < NR_POINT_LIGHTS; ++i)
+	{
+		result += CalcPointLight(pointLights[i], norm, FragPos, viewDir);
+	}
+
+	result += CalcSpotLight(spotLight, norm, FragPos, viewDir);
+
+	Fragcolor = vec4(result, 1.0f);
+}
