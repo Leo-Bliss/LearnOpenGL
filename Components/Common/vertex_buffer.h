f0e7d75eacbc23941bdf58dc46e2db8ab13918bd
laizhihao <tbyouth11@gmail.com>
2022-03-27 16:46:19 +0800

    refactor: 提取出buffer共用部分

--- Components/Common/vertex_buffer.h
+++ Components/Common/vertex_buffer.h
@@ -1,25 +1,10 @@
 #pragma once
 #include "utils.h"
 #include "define.h"
+#include "buffer.h"
 
 namespace Hub
 {
-	namespace BufferUsage
-	{
-		enum buffer_usage_t
-		{
-			StreamDraw = GL_STREAM_DRAW,
-			StreamRead = GL_STREAM_READ,
-			StreamCopy = GL_STREAM_COPY,
-			StaticDraw = GL_STATIC_DRAW,
-			StaticRead = GL_STATIC_READ,
-			StaticCopy = GL_STREAM_COPY,
-			DynamicDraw = GL_DYNAMIC_DRAW,
-			DynamicRead = GL_DYNAMIC_READ,
-			DynamicCopy = GL_DYNAMIC_COPY
-		};
-	}
-
 	SharedPtrDefine(VertexBuffer);
 
 	class VertexBuffer
