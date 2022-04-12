f84ffad9aa95727f3826efc676c8ae4306b5089f
tbyouth <tbyouth11@gmai.com>
2022-04-12 23:54:40 +0800

    test: frambuffer应用只后处理加权平均灰度效果

--- FrameBuffer/shader/framebuffer_screen.fs
+++ FrameBuffer/shader/framebuffer_screen.fs
@@ -8,6 +8,9 @@ uniform sampler2D screenTexture;
 void main()
 {
 	vec4 texColor = texture(screenTexture, TexCoords);
-	float average = (texColor.r + texColor.g + texColor.b) / 3.0;
+	float redWeight = 0.2126;
+	float greenWeight = 0.7152;
+	float blueWeight = 1.0 - redWeight - greenWeight;
+	float average = (redWeight * texColor.r + greenWeight * texColor.g + blueWeight * texColor.b) / 3.0;
 	FragColor = vec4(vec3(average), 1.0);
 }
