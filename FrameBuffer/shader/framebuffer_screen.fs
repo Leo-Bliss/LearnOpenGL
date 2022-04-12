31c67b268ccf7aa01e2e75a5377aaccec5a540f4
tbyouth <tbyouth11@gmai.com>
2022-04-13 00:15:21 +0800

    test: framebuffer应用后处理之kernel effects
    获取周围纹理坐标然后采样取周围对应纹理值

--- FrameBuffer/shader/framebuffer_screen.fs
+++ FrameBuffer/shader/framebuffer_screen.fs
@@ -4,13 +4,49 @@ out vec4 FragColor;
 in vec2 TexCoords;
 
 uniform sampler2D screenTexture;
+const float offset = 1.0 / 300.0;
 
 void main()
 {
-	vec4 texColor = texture(screenTexture, TexCoords);
-	float redWeight = 0.2126;
-	float greenWeight = 0.7152;
-	float blueWeight = 1.0 - redWeight - greenWeight;
-	float average = (redWeight * texColor.r + greenWeight * texColor.g + blueWeight * texColor.b) / 3.0;
-	FragColor = vec4(vec3(average), 1.0);
+    vec2 offsets[9] = vec2[](
+            vec2(-offset,  offset), // top-left
+            vec2( 0.0f,    offset), // top-center
+            vec2( offset,  offset), // top-right
+            vec2(-offset,  0.0f),   // center-left
+            vec2( 0.0f,    0.0f),   // center-center
+            vec2( offset,  0.0f),   // center-right
+            vec2(-offset, -offset), // bottom-left
+            vec2( 0.0f,   -offset), // bottom-center
+            vec2( offset, -offset)  // bottom-right    
+        );
+    
+    /*
+    float kernel[9] = float[](
+        -1, -1, -1,
+        -1,  9, -1,
+        -1, -1, -1
+    );*/
+
+    /*
+    // Blur
+    float kernel[9] = float[](
+    1.0 / 16, 2.0 / 16, 1.0 / 16,
+    2.0 / 16, 4.0 / 16, 2.0 / 16,
+    1.0 / 16, 2.0 / 16, 1.0 / 16  
+    );*/
+    // Edge detection
+    float kernel[9] = float[](
+        1, 1, 1,
+        1, -8, 1,
+        1, 1, 1
+    );
+
+    vec3 sampleTex[9];
+    vec3 color = vec3(0.0);
+    for(int i = 0; i < 9; ++i)
+    {
+        sampleTex[i] = texture(screenTexture, TexCoords.st + offsets[i]).grb;
+        color += sampleTex[i] * kernel[i];
+    }
+    FragColor = vec4(color, 1.0);
 }
