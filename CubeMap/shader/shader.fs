23e906d2a68cd06200ea47d32e6c3abb588f728f
tbyouth <tbyouth11@gmail.com>
2022-05-05 00:03:37 +0800

    test: 环境映射折射效果

--- CubeMap/shader/shader.fs
+++ CubeMap/shader/shader.fs
@@ -8,7 +8,8 @@ uniform samplerCube skybox;
 
 void main()
 {
+	float ratio = 1.00 / 1.52; // refraction ratio from air to glass
 	vec3 I = normalize(Position - cameraPos);
-	vec3 R = reflect(I, normalize(Normal));
+	vec3 R = refract(I, normalize(Normal), ratio);
 	FragColor = vec4(texture(skybox, R).rgb, 1.0);
 }
