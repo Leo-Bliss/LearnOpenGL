642344d10dbcd206f7a307d781baa25e0c377b46
tbyouth <tbyouth11@gmail.com>
2022-06-21 00:50:13 +0800

    style： 调整代码风格

--- Shadow/shader/cube_mapping_depth.fs
+++ Shadow/shader/cube_mapping_depth.fs
@@ -2,13 +2,13 @@
 in vec4 FragPos;
 
 uniform vec3 lightPos;
-uniform float far;
+uniform float far_plane;
 
 void main()
 {
 	float lightDistance = length(FragPos.xyz - lightPos);
 
-	lightDistance /= far;
+	lightDistance /= far_plane;
 
 	gl_FragDepth = lightDistance;
 }
